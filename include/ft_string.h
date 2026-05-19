893b26728d28f2f9202f323d9e1f6be615b814e9
Nicholas Dymov <NicholasDymov@users.noreply.github.com>
2026-05-19 16:38:09 +0700

    Header updates

--- include/ft_string.h
+++ include/ft_string.h
@@ -6,7 +6,7 @@
 /*   By: ndymov <ndymov@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 10:40:18 by ndymov            #+#    #+#             */
-/*   Updated: 2026/05/12 19:05:05 by ndymov           ###   ########.fr       */
+/*   Updated: 2026/05/19 16:36:13 by ndymov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,7 @@
 # include "libft.h"
 # include <stddef.h> // size_t
 
+int		ft_isspace(int c);
 size_t	ft_strlen(const char *str);
 char	*ft_strcpy(char *restrict dst, const char *restrict src);
 char	*ft_strncpy(char *restrict dst, const char *restrict src, size_t n);
--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: ndymov <ndymov@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 09:48:48 by ndymov            #+#    #+#             */
-/*   Updated: 2026/05/11 10:13:48 by ndymov           ###   ########.fr       */
+/*   Updated: 2026/05/19 15:49:16 by ndymov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,9 @@ typedef enum e_error
 {
 	OK,
 	ERR_INVAL,
-	ERR_NOMEM
+	ERR_NOMEM,
+	ERR_EOF,
+	ERR_UNCLOSED_QUOTE
 }	t_error;
 
 #endif
