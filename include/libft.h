9d7d78500cd7d626ac1d504606a87af803b4b426
Nicholas Dymov <NicholasDymov@users.noreply.github.com>
2026-05-20 17:40:07 +0700

    Header updates

--- include/libft.h
+++ include/libft.h
@@ -6,7 +6,7 @@
 /*   By: ndymov <ndymov@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 09:48:48 by ndymov            #+#    #+#             */
-/*   Updated: 2026/05/19 15:49:16 by ndymov           ###   ########.fr       */
+/*   Updated: 2026/05/20 16:13:43 by ndymov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,8 @@ typedef enum e_error
 	ERR_INVAL,
 	ERR_NOMEM,
 	ERR_EOF,
-	ERR_UNCLOSED_QUOTE
+	ERR_UNCLOSED_QUOTE,
+	ERR_PARSE,
 }	t_error;
 
 #endif
