8a6755e0ab345a3b99246ae2ddddabf27bb4449e
Nicholas Dymov <NicholasDymov@users.noreply.github.com>
2026-05-26 18:07:07 +0700

    ft_error.h update

--- include/ft_error.h
+++ include/ft_error.h
@@ -6,7 +6,7 @@
 /*   By: ndymov <ndymov@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 09:48:48 by ndymov            #+#    #+#             */
-/*   Updated: 2026/05/21 09:02:47 by ndymov           ###   ########.fr       */
+/*   Updated: 2026/05/26 18:06:18 by ndymov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ typedef enum e_error
 	OK,
 	ERR_INVAL,
 	ERR_NOMEM,
+	ERR_EXECVE,
 	ERR_EOF,
 	ERR_UNCLOSED_QUOTE,
 	ERR_PARSE,
