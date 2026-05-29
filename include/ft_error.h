759c25e58caabada1bf80afa4617b2681a338381
Nicholas Dymov <NicholasDymov@users.noreply.github.com>
2026-05-30 00:35:01 +0700

    ft_error.h updates

--- include/ft_error.h
+++ include/ft_error.h
@@ -6,7 +6,7 @@
 /*   By: ndymov <ndymov@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/05/11 09:48:48 by ndymov            #+#    #+#             */
-/*   Updated: 2026/05/29 18:26:31 by ndymov           ###   ########.fr       */
+/*   Updated: 2026/05/30 00:25:24 by ndymov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,8 @@ typedef enum e_error
 	ERR_CLOSE,
 	ERR_DUP,
 	ERR_ACCESS,
+	ERR_NOENT,
+	ERR_NOCMD,
 	ERR_EXECVE,
 	ERR_GETCWD,
 	ERR_CHDIR,
