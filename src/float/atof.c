d8f2d3548fd4f221a5b9d13bdce2c5125e5dbd31
Nicholas Dymov <NicholasDymov@users.noreply.github.com>
2026-07-10 15:02:42 +0700

    safe_atof function stub added

--- src/float/atof.c
+++ src/float/atof.c
@@ -6,7 +6,7 @@
 /*   By: ndymov <ndymov@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/10 14:47:32 by ndymov            #+#    #+#             */
-/*   Updated: 2026/07/10 14:54:02 by ndymov           ###   ########.fr       */
+/*   Updated: 2026/07/10 15:02:17 by ndymov           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,16 @@
 float	ft_atof(const char *s);
 double	ft_atod(const char *s);
 
-t_error	ft_safe_atof(const char *s, float *x);
-t_error	ft_safe_atod(const char *s, double *x);
+t_error	ft_safe_atof(const char *s, float *x)
+{
+	(void)s;
+	(void)x;
+	return (OK);
+}
+
+t_error	ft_safe_atod(const char *s, double *x)
+{
+	(void)s;
+	(void)x;
+	return (OK);
+}
